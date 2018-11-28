55c47d32718f92520254640ecb3535a60f3bf13f
sun <sunchangjunn@outlook.com>
2018-11-28 15:36:29 +0800

    定时任务

--- src/main/java/com/sun/project/task/TaskTest.java
+++ src/main/java/com/sun/project/task/TaskTest.java
@@ -8,14 +8,26 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class TaskTest {
-	
-	   @Scheduled(cron="0 15 12 28 11 ?") //cron表达式
-	    public void doSomething() {
+		/*@
+		 * Scheduled(fixedDelay = 5000)
+		 * 相同速率 每5秒调用一次
+		 * */
+		@Scheduled(fixedDelay = 5000) 
+	    public void doSomething() throws InterruptedException {
 	        System.out.println("执行方法:doSomething");
+	        Thread.sleep(15000);
 	    }
-	   @Scheduled(cron="0 15 12 28 11 ?") //cron表达式
-	   public void doSomethingTest() {
+//	   @Scheduled(cron="0 15 12 28 11 ?") //cron表达式
+		@Scheduled(fixedDelay = 5000) 
+	   public void doSomethingTest() throws InterruptedException {
 		   System.out.println("执行方法:doSomethingTest");
+		   Thread.sleep(25000);
 	   }
+//		@Scheduled(fixedDelay = 5000) 
+		public void doSomethingTestOne() throws InterruptedException {
+			System.out.println("执行方法:doSomethingTest>>>>>>>>>>>>>>>>");
+			 Thread.sleep(25000);
+			 
+		}
 
 }
